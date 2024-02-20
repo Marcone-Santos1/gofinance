171b44a847369d9c85e567924cf8d7270504d483
Marcone Santos <98716688+Marcone-Santos1@users.noreply.github.com>
2024-02-19 21:59:10 -0300

    add styled components

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { Container, Title } from "./styles";
+
+export const Dashboard = () => {
+    return (
+        <Container>
+            <Title>Dashboard</Title>
+        </Container>
+    );
+}
